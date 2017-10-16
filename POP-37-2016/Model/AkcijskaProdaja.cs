6239690594eaa7f864b2a7c88afd79ee07ecc5f9
student <student@JD1-16>
2017-10-16 13:25:56 +0200

    Model

---
+++ POP-37-2016/Model/AkcijskaProdaja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_37_2016.Model
+{
+    public class AkcijskaProdaja
+    {
+        public int Id { get; set; }
+        public string NamestajNaPopustu { get; set; }
+        public double Popust { get; set; }
+        public DateTime DatumPocetka { get; set; }
+        public DateTime DatumZavrsetka { get; set; }
+        public double CenaSaPopustom { get; set; }
+        public bool Obrisan { get; set; }
+    }
+}
